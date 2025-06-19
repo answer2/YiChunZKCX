2aa8ae6072cbc2dc9dd1d2aa5068000f095de3cf
AnswerDev <62866043+answer2@users.noreply.github.com>
2025-06-19 20:52:37 +0800

    Update AppConfig.java

--- app/src/main/java/dev/answer/yichunzkcx/AppConfig.java
+++ app/src/main/java/dev/answer/yichunzkcx/AppConfig.java
@@ -5,11 +5,11 @@ public class AppConfig {
     
     public static final boolean isSupport = Build.VERSION.SDK_INT < 34;
     
-    public static final int year = 2024;
+    public static final int year = 2025;
     
-    public static final String endTime_str = "2024-07-02 09:00:00";
+    public static final String endTime_str = "2025-07-02 09:00:00";
     
     public static final String app_name = year + "宜春中考成绩查询";
     
-    public static final String app_version = "1.0.3-bata";
+    public static final String app_version = "1.0.3";
 }
